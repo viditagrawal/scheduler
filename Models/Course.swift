cb763b9b83ce4356f9ee109dcc195896f8026302
CollinQ <collinqian@ucsb.edu>
2023-04-22 03:00:38 -0700

    course model + ucsb gold api parsing

---
+++ Models/Course.swift
@@ -0,0 +1,19 @@
+//
+//  Course.swift
+//  scheduler
+//
+//  Created by Collin Qian on 4/22/23.
+//
+
+import Foundation
+
+struct Course : Decodable {
+    var title: String
+    var description: String
+    var instructor: String
+    var beginTime: String
+    var endTime: String
+    var unitsFixed: Int
+    var courseID: String
+    var enrollCode: String
+}
