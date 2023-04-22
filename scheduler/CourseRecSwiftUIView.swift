5fa6ebb7582164c2512d9613e36e25d391e63def
Justin Cao <justinthecao@gmail.com>
2023-04-22 16:16:45 -0700

    pushing

--- scheduler/CourseRecSwiftUIView.swift
+++ scheduler/CourseRecSwiftUIView.swift
@@ -19,6 +19,8 @@ struct ContentView: View {
     }
 }
 
+var courses : [String] = []
+
 struct CourseRecSwiftUIView: View {
     @State private var text = ""
     var body: some View {
@@ -40,15 +42,30 @@ struct CourseRecSwiftUIView: View {
                 let task = session.dataTask(with: request) { (data, response, error) in
                     if let data = data, let dataString = String(data: data, encoding: .utf8)
                     {
-                        print(dataString)
+                        //print(type(of:dataString))
+                        if let jsonData = dataString.data(using: .utf8){
+                            
+                            let json = try! JSONSerialization.jsonObject(with: jsonData, options: [.allowFragments]) as! [String: String]
+                            
+                            // extract the non-decimal values into an array
+                            var result = [String]()
+                            for (_, value) in json {
+                                print(value)
+                                result.append(value)
+                            }
+                            print(dataString)
+                            print(result)
+                        }
+                        if let error = error {
+                            // Handle HTTP request error
+                        } else if data != nil {
+                            // Handle HTTP request response
+                        } else {
+                            // Handle unexpected error
+                        }
                     }
-                    if let error = error {
-                        // Handle HTTP request error
-                    } else if let data = data {
-                        // Handle HTTP request response
-                    } else {
-                        // Handle unexpected error
-                    }
+                    
+                    
                 }
                 task.resume()
                 
@@ -61,12 +78,36 @@ struct CourseRecSwiftUIView: View {
                 .stroke(Color.gray, lineWidth: 1)
             )
             .shadow(radius: 2)
+            Spacer(minLength: 50)
+            
             Spacer(minLength: 600)
         }
         
         
     }
 }
+
+
+//
+//var courseView = CourseRecSwiftUIView()
+//var observedArray: [String] = [] {
+//    didSet {
+//        // Loop through the elements in the array
+//        for element in observedArray {
+//            // Check if the element already has a corresponding label in the view
+//            if myView.viewWithTag(element.hashValue) == nil {
+//                // If not, create a new label with the element as the text and add it to the view
+//                let newLabel = UILabel()
+//                newLabel.text = element
+//                newLabel.tag = element.hashValue
+//                myView.addSubview(newLabel)
+//            }
+//        }
+//    }
+//}
+//
+//observedArray = myArray
+
 
 struct CourseRecSwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
