cf3312c8ab23130e9f6cc12c163b75547935eb42
viditagrawal <40546427+viditagrawal@users.noreply.github.com>
2023-04-22 03:57:16 -0700

    new updates

---
+++ scheduler/HomeSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  scheduler
+//
+//  Created by vidit agrawal on 4/22/23.
+//
+
+import SwiftUI
+
+struct HomeSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeSwiftUIView()
+    }
+}
