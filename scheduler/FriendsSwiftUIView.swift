4ed7474ec6fd6de12901faa4792686d214b0e0f0
Justin Cao <64385380+justinthecao@users.noreply.github.com>
2023-04-22 23:47:19 -0700

    Update FriendsSwiftUIView.swift

--- scheduler/FriendsSwiftUIView.swift
+++ scheduler/FriendsSwiftUIView.swift
@@ -6,15 +6,66 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
-struct FriendsSwiftUIView: View {
+struct DetailView: View {
+    var item: String
+    
     var body: some View {
-        Text("friends")
+        VStack {
+            Text(item)
+                .font(.title)
+                .padding()
+            Text("This is the detail view for \(item).")
+                .padding()
+        }
+        .navigationTitle(item)
     }
 }
+
+
+//filled with uuid's
+//var recommended = []
+
+//friends you are already friends with
+//var friends = []
+
+struct FriendsSwiftUIView: View {
+    @State private var searchText = ""
+    @State private var selectedChoice = ""
+    let choices = ["friend1", "friend2", "friend3", "friend4"]
+    var filteredItems: [String] {
+        if searchText.isEmpty {
+            return choices
+        } else {
+            return choices.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    var body: some View {
+        NavigationView {
+            VStack{
+                TextField("Search", text: $searchText)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .padding()
+                List(filteredItems, id: \.self) { item in
+                                NavigationLink(destination: DetailView(item: item)) {
+                                    Text(item)
+                                }
+                        }
+                Spacer()
+            }.navigationTitle("Search")
+        }
+    }
+}
+
 
 struct FriendsSwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
         FriendsSwiftUIView()
+        
+        
+        
+        
     }
 }
+
