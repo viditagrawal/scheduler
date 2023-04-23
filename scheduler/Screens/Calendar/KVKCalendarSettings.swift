eeab0af43208de78d04bb876f62f12d22569e826
CollinQ <collinqian@ucsb.edu>
2023-04-23 01:34:15 -0700

    no more break

--- scheduler/Screens/Calendar/KVKCalendarSettings.swift
+++ scheduler/Screens/Calendar/KVKCalendarSettings.swift
@@ -93,7 +93,7 @@ extension KVKCalendarSettings where Self: KVKCalendarDataModel {
         let decoder = JSONDecoder()
         var courses : Set<String> = []
         if let curruser = Auth.auth().currentUser {
-            //let uid = curruser.uid
+            let uid = curruser.uid
             //let uid = "PeT7yg3UqCRbf3CkjoAJoFv0l8z2"
             let db = Firestore.firestore()
             let currData = db.collection("data").document(uid)
